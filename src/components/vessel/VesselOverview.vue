80442d05929231fad2b43640eb2b7928c518aa0e
Sidney Pauly <sidney.pauly@venios.de>
2023-10-13 17:28:49 +0100

    Made vessel overview scrollable

--- src/components/vessel/VesselOverview.vue
+++ src/components/vessel/VesselOverview.vue
@@ -1,33 +1,34 @@
 <template>
+    <div class="vessel-list">
 
-    <v-table>
-        <thead>
-            <tr>
+        <v-table>
+            <thead>
+                <tr>
 
-                <th class="text-left">
-                    Name
-                </th>
-                <th class="text-left">
-                    Version
-                </th>
-                <th class="text-left">
-                    Num. Parts
-                </th>
-                <th class="text-left">
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr  v-for="(item, key) of vessels" :key="key">
-                <td><b>{{ item.entity?.name }}</b></td>
-                <td>{{ item.entity?._version }}</td>
-                <td>{{ item.entity?.parts.length }}</td>
+                    <th class="text-left">
+                        Name
+                    </th>
+                    <th class="text-left">
+                        Version
+                    </th>
+                    <th class="text-left">
+                        Num. Parts
+                    </th>
+                    <th class="text-left">
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(item, key) of vessels" :key="key">
+                    <td><b>{{ item.entity?.name }}</b></td>
+                    <td>{{ item.entity?._version }}</td>
+                    <td>{{ item.entity?.parts.length }}</td>
 
-                <td><v-btn @click="router.push(`./vessel/details/${item.entity?._id}`)">Flights</v-btn></td>
-            </tr>
-        </tbody>
-    </v-table>
-
+                    <td><v-btn @click="router.push(`./vessel/details/${item.entity?._id}`)">Flights</v-btn></td>
+                </tr>
+            </tbody>
+        </v-table>
+    </div>
 </template>
 
 <script setup lang="ts">
@@ -39,8 +40,15 @@ import { useRouter } from 'vue-router';
 
 const router = useRouter()
 
-const vessels = useObservableShallow(getVessels(), {initialValue: undefined})
+const vessels = useObservableShallow(getVessels(), { initialValue: undefined })
 
 fetchVesselsIfNecessary()
 
 </script>
+
+<style lang="scss">
+.vessel-list {
+    max-height: 80vh;
+    overflow-y: scroll;
+}
+</style>
