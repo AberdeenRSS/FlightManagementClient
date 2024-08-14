ededac2b1a73637a48b41bb7878ff423a65c4e60
Alan Fleming <114114685+alanjsfleming@users.noreply.github.com>
2024-08-14 23:25:01 +0100

    auth code will clear now and not overfill the screen. Also changed component list to a ul

--- src/views/VesselDetails.vue
+++ src/views/VesselDetails.vue
@@ -1,5 +1,5 @@
 <template>
-    <div v-if="!!vessel" style="height: 100%; flex-direction: column;" class="d-flex ">
+    <div v-if="!!vessel" style="height: 100%; flex-direction: column;" class="d-flex flex-column">
 
         <div>
             <div class="ma-2 font-weight-light text-h3">
@@ -7,24 +7,34 @@
             </div>
 
         </div>
-        <v-card>
-            <v-card-title v-if="token">Auth Code</v-card-title>
-            <v-alert v-if="tokenError" :text="tokenError" type="error"></v-alert>
-            
-            <v-card-text v-if="token">{{ token }}</v-card-text>
-            <v-card-actions>
-                <v-btn @click="createAuthCode" :loading="tokenLoading">Create Auth Code</v-btn>
-                <v-btn @click="copyAuthToken" v-if="token">Copy</v-btn>
-            </v-card-actions>
-        </v-card>
+        <div>
+            <h3>Actions</h3>
+            <div class="d-flex flex-row">
+                <div class="d-flex flex-column" style="max-width:400px">
+                    <v-alert v-if="tokenError" :text="tokenError" type="error"></v-alert>
+
+                    <div v-if="token">
+                        <p style="overflow-wrap: break-word;padding:1rem;">{{ token }}</p>
+
+                    </div>
+                    <div class="mx-auto">
+                        <v-btn @click="createAuthCode" :loading="tokenLoading" v-if="!token">Create Auth Code</v-btn>
+                        <v-btn @click="copyAuthToken" v-if="token">Copy Code</v-btn>
+                        <v-btn @click="clearAuthToken" v-if="token">Clear</v-btn>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
 
         <div class="d-flex" style="min-height: 1px;">
-            <div style="flex-basis: 200px; flex-grow: 0.4;">
-                <VesselChart v-model="selected" :vessel-id="id"></VesselChart>
-            </div>
-
             <div style="max-height: 100%; overflow-y: scroll; flex-grow: 1;">
                 <FlightList :vessel-id="id"></FlightList>
+            </div>
+
+            <div style="flex-basis: 200px; flex-grow: 0.4;">
+                <VesselComponentsList v-model="selected" :vessel-id="id"></VesselComponentsList>
             </div>
         </div>
 
@@ -36,7 +46,6 @@
 </template>
 
 <script setup lang="ts">
-import VesselChart from '@/components/vessel/VesselChart.vue';
 import FlightList from '@/components/flights/FlightList.vue';
 import { useRoute } from 'vue-router';
 import { ref } from 'vue';
@@ -48,6 +57,7 @@ import { useUser } from '@/composables/auth/useUser';
 import axios from 'axios';
 import { useRssApiBaseUri } from '@/composables/api/rssFlightServerApi';
 import { until } from '@vueuse/core';
+import VesselComponentsList from '@/components/vessel/VesselComponentsList.vue';
 
 const route = useRoute()
 const id = route.params.id as string
@@ -70,6 +80,10 @@ function copyAuthToken() {
     alert("Successfully copied token!")
 }
 
+function clearAuthToken() {
+    token.value = undefined
+}
+
 async function createAuthCode() {
 
     tokenError.value = undefined
@@ -78,14 +92,15 @@ async function createAuthCode() {
     const loadedVessel = await until(vessel).toBeTruthy()
     const user = await until(currentUser).toBeTruthy()
 
-    const validUntil = new Date(Date.now() + 1000*60*60*24*60)
+    const validUntil = new Date(Date.now() + 1000 * 60 * 60 * 24 * 60)
 
     try {
-        const res = await axios.post(`${useRssApiBaseUri()}/vessel/create_auth_code/${loadedVessel._id}/${validUntil.toISOString()}`, undefined, {headers: {'Authorization': user.jwt_token}})
+        const res = await axios.post(`${useRssApiBaseUri()}/vessel/create_auth_code/${loadedVessel._id}/${validUntil.toISOString()}`, undefined, { headers: { 'Authorization': user.jwt_token } })
         token.value = res.data['_id']
     }
     catch (e) {
         tokenError.value = String(e)
+        tokenLoading.value = false
     }
     finally {
         tokenLoading.value = false
